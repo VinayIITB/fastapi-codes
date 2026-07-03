a965d4aadf6682a8bcb3656cfdfe9bd7cb1f0931
VINAYIITB <gvinay2k24@gmail.com>
2026-07-04 02:13:49 +0530

    error handing done

--- FastAPI/status_code.py
+++ FastAPI/status_code.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,status
+from fastapi import FastAPI,status,HTTPException
 app=FastAPI()
 
 @app.post("/create_user",status_code=status.HTTP_201_CREATED)
@@ -6,4 +6,28 @@ app=FastAPI()
 def create_user():
     return{
         "message":"user created"
+    }
+    
+@app.get("/user")
+def user():
+    return{
+        "status":"sucess",
+        "message":"user fetched",
+        "data":{
+            "name":"vinay",
+            "age":24
+        }
+    }
+
+# it is for error handling and this get api is for user id
+@app.get("/users/{user_id}")
+def get_staus(user_id:int):
+    if user_id!=1:
+        raise HTTPException(
+            status_code=404,
+            detail="users not found"
+        )
+    return{
+        "id":1,
+        "name":"Mohit"
     }
